777fefc611e79d8933b4bd3bc7bb8f615d99603d
nickwilly4 <williamaichholz@gmail.com>
2023-06-17 20:33:12 -0600

    Made comments easier to read

--- Source/MixerComponent.h
+++ Source/MixerComponent.h
@@ -38,23 +38,75 @@ private:
     //==============================================================================
     // Your private member variables go here...
     
+    // ========Channel 1========
+    // Load
     juce::TextButton load_chn1;
-    
+    // Dials
     juce::Slider trim_chn1;
     juce::Slider hi_chn1;
     juce::Slider med_chn1;
     juce::Slider lo_chn1;
     juce::Slider clr_chn1;
     juce::Slider fx_chn1;
-    
+    // Toggle Buttons
     juce::TextButton fxtoggle_chn1;
     juce::TextButton cuetoggle_chn1;
+    // Volume Fader
+    juce::Slider volFader_chn1;
+    // ========End Channel 1========
     
-    // Volume fader
-    juce::Slider volFader;
+    // ========Channel 2========
+    // Load
+    juce::TextButton load_chn2;
+    // Dials
+    juce::Slider trim_chn2;
+    juce::Slider hi_chn2;
+    juce::Slider med_chn2;
+    juce::Slider lo_chn2;
+    juce::Slider clr_chn2;
+    juce::Slider fx_chn2;
+    // Toggle Buttons
+    juce::TextButton fxtoggle_chn2;
+    juce::TextButton cuetoggle_chn2;
+    // Volume Fader
+    juce::Slider volFader_chn2;
+    // ========End Channel 2========
     
-    //std::unique_ptr<juce::FileChooser> chooser;
-    //juce::AudioFormatManager formatManager;
+    // ========Channel 3========
+    // Load
+    juce::TextButton load_chn3;
+    // Dials
+    juce::Slider trim_chn3;
+    juce::Slider hi_chn3;
+    juce::Slider med_chn3;
+    juce::Slider lo_chn3;
+    juce::Slider clr_chn3;
+    juce::Slider fx_chn3;
+    // Toggle Buttons
+    juce::TextButton fxtoggle_chn3;
+    juce::TextButton cuetoggle_chn3;
+    // Volume Fader
+    juce::Slider volFader_chn3;
+    // ========End Channel 3========
+    
+    // ========Channel 4========
+    // Load
+    juce::TextButton load_chn4;
+    // Dials
+    juce::Slider trim_chn4;
+    juce::Slider hi_chn4;
+    juce::Slider med_chn4;
+    juce::Slider lo_chn4;
+    juce::Slider clr_chn4;
+    juce::Slider fx_chn4;
+    // Toggle Buttons
+    juce::TextButton fxtoggle_chn4;
+    juce::TextButton cuetoggle_chn4;
+    // Volume Fader
+    juce::Slider volFader_chn4;
+    // ========End Channel 4========
+    
+    
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MixerComponent)
     
