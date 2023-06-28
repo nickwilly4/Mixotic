559a0ab87feb3954d26b0a2de7512acdc136cc80
nickwilly4 <williamaichholz@gmail.com>
2023-06-28 14:56:35 -0600

    Cleaned code

--- Source/EQControlColumn.cpp
+++ Source/EQControlColumn.cpp
@@ -96,20 +96,11 @@ EQControlColumn::~EQControlColumn()
 //==============================================================================
 void EQControlColumn::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    //g.setFont (juce::Font (48.0f));
-    //g.setColour (juce::Colours::green);
-    //g.drawText ("New Graphics!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void EQControlColumn::resized()
 {
-    // This is called when the MainComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.\
-    
     // Convenient
     auto margin = 10;
     auto space = 10;
--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,20 +16,13 @@ MainComponent::MainComponent()
 }
 MainComponent::~MainComponent()
 {
-    // This shuts down the audio device and clears the audio source.
     shutdownAudio();
 }
 
 //==============================================================================
 void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
 {
-    // This function will be called when the audio device is started, or when
-    // its settings (i.e. sample rate, block size, etc) are changed.
-
-    // You can use this function to initialise any resources you might need,
-    // but be careful - it will be called on the audio thread, not the GUI thread.
-
-    // For more details, see the help for AudioProcessor::prepareToPlay()
+    
 }
 
 void MainComponent::onEQControlColumnLoadClicked(const std::string& columnNumber) {
@@ -39,21 +32,12 @@ void MainComponent::onEQControlColumnLoadClicked(const std::string& columnNumber
 
 void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
 {
-    // Your audio-processing code goes here!
-
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
     bufferToFill.clearActiveBufferRegion();
 }
 
 void MainComponent::releaseResources()
 {
-    // This will be called when the audio device stops, or when it is being
-    // restarted due to a setting change.
-
-    // For more details, see the help for AudioProcessor::releaseResources()
+    
 }
 
 //==============================================================================
@@ -64,8 +48,6 @@ void MainComponent::paint (juce::Graphics& g)
 
 void MainComponent::resized()
 {
-    //auto area = getLocalBounds();
-    //auto deckAreaWidth = area.getWidth() / 3;
     leftDeckComponent.setBounds(0, 0, leftDeckComponent.getWidth(), leftDeckComponent.getHeight());
     rightDeckComponent.setBounds(leftDeckComponent.getWidth()+80*4, 0, rightDeckComponent.getWidth(), leftDeckComponent.getHeight());
     
